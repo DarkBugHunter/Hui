d51163bb32b9cfffd77523664fdb5dd706e6dae2
DarkBugHunter <78596168+DarkBugHunter@users.noreply.github.com>
2021-02-11 22:56:03 +0530

    Update dup.py

--- dup.py
+++ dup.py
@@ -18,10 +18,13 @@ txtFiles = []
 for files in glob.glob("*.txt"):
     txtFiles.append(files)
 
+if len(txtFiles) == 0:
+    print("Oops No txt Files Found")
+    exit()
+    
 print(f"Found {len(txtFiles)} txt Files")
 
-fileName = 1
-outFile = 1
+
 folderPath = "/home/darkweb/Bug/Cleaned Files/"
 
 for i in range(len(txtFiles)):
@@ -29,9 +32,9 @@ for i in range(len(txtFiles)):
     outfile2 = open(newFileout, "w")
     infile = open(txtFiles[i], "r")
     for line in infile:
-        if line not in linesSet:
+        if line.lower() not in linesSet:
             outfile2.write(line)
-            linesSet.add(line)
+            linesSet.add(line.lower())
     outfile2.close()
     
 print("Done")
